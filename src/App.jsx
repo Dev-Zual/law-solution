acc2111708a1a9e1d9a8c41c1d470449abe8bb28
Zual Rana <freelancerzual@gmail.com>
2024-12-09 16:19:38 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <h1 className="text-5xl">tailwind</h1>
+        <p>poppins font</p>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
